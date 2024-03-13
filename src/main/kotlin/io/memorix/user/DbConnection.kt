b4e12318fd274ae691eb17d2f30109d385e601f3
jelleriecker <jelle.riecker@nelen-schuurmans.nl>
2024-03-13 15:30:32 +0100

    fixed get request, now to work on contains

--- src/main/kotlin/io/memorix/user/DbConnection.kt
+++ src/main/kotlin/io/memorix/user/DbConnection.kt
@@ -1,16 +1,15 @@
 package io.memorix.user
 
-import org.jetbrains.exposed.dao.id.IntIdTable
 import org.jetbrains.exposed.sql.Database
 import org.jetbrains.exposed.sql.SchemaUtils
+import org.jetbrains.exposed.sql.Table
 import org.jetbrains.exposed.sql.transactions.transaction
 
-object Users : IntIdTable() {
+object Users : Table() {
     val email = text("email").uniqueIndex()
     val name = text("name")
     val password_hash = text("password_hash")
 }
-
 
 fun initDatabase() {
 //    val dotenv = Dotenv.configure().load() //TODO FIX .env references
@@ -27,6 +26,6 @@ fun initDatabase() {
     )
     transaction {
         // Create tables if they don't exist
-        SchemaUtils.create(UsersTables.Users)
+        SchemaUtils.create(Users)
     }
 }
